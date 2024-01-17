44ac0cbfcacc72703da0b592545262d9813c02ea
fazimi5 <76415233+fazimi5@users.noreply.github.com>
2024-01-17 13:45:18 -0600

    ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ec2_ins_farhan" {
 
   ami                         = "ami-0905a3c97561e0b69"
   instance_type               = "t2.micro"
-  vpc_security_group_ids      =  [aws_security_group.aws_sg.id]
+#  vpc_security_group_ids      =  [aws_security_group.aws_sg.id]
   associate_public_ip_address = true
   key_name                    = "farhan_public_key1" # your key here
 
@@ -14,17 +14,17 @@ resource "aws_instance" "ec2_ins_farhan" {
   }
 }
 ## create security group
-resource "aws_security_group" "aws_sg" {
-  name = "security group from terraform by farhan"
+#resource "aws_security_group" "aws_sg" {
+#  name = "security group from terraform by farhan"
 
 
-  ingress {
-    description = "SSH from the internet"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+ # ingress {
+ #   description = "SSH from the internet"
+ #   from_port   = 22
+ #   to_port     = 22
+ #   protocol    = "tcp"
+ #   cidr_blocks = ["0.0.0.0/0"]
+ # }
 
 #  ingress {
 #   description = "80 from the internet"
@@ -34,12 +34,12 @@ resource "aws_security_group" "aws_sg" {
 #    cidr_blocks = ["0.0.0.0/0"]
 #  }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+ # egress {
+ #   from_port   = 0
+ #   to_port     = 0
+ #   protocol    = "-1"
+ #   cidr_blocks = ["0.0.0.0/0"]
+ # }
   
 } 
 
